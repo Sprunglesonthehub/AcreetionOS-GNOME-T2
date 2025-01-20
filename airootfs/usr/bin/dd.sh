3005d4a85bd186358f5e163795fe512b01333f45
Johnathan Spiva <sprungles.me@tuta.io>
2025-01-19 20:58:22 -0800

    Initial Commit. VMLinuz-linux issues.

---
+++ airootfs/usr/bin/dd.sh
@@ -0,0 +1,4 @@
+dconf load /org/gnome/ < /gnome-settings.dconf
+
+rm $HOME/.config/autostart/dd.desktop
+
---
+++ build.sh
@@ -0,0 +1,2 @@
+ rm -rf work/ && rm -rf out/ && sudo /mnt/drives/Projects/AcreetionOS-GNOME-T2/mkarchiso -L AcreetionOS-GNOME-T2 -v -o ../ISO . -j44 && sudo rm -rf ./work
+
